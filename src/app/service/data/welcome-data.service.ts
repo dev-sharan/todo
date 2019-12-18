214c6c42baf8293f426c9f84aebf3655dc0caa06
dev-sharan <sharan.manish@ymail.com>
2019-12-18 13:31:05 +0000

    data/wecome data added

---
+++ src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WelcomeDataService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  executeHelloWorldBeanService() {
+    return this.httpClient.get('https://8080-f5233174-1bd3-4077-b0a1-0a846cbf243c.ws-ap01.gitpod.io/hello-world-bean');
+  }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,11 +8,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  message: any;
 
-  constructor(private actRoute: ActivatedRoute) { }
+  constructor(private actRoute: ActivatedRoute , private welcomeDataService: WelcomeDataService) { }
 
   ngOnInit() {
     console.log(this.actRoute.snapshot.params['name']);
   }
 
+  getWelcomeMsg() {
+    this.welcomeDataService.executeHelloWorldBeanService().subscribe(
+      res => {
+        this.message = res['message'];
+      }
+    )
+  }
 }
